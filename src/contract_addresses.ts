1ee40fd5d8b1e9af296949f5c3be10f9489c7838
jwalin12 <jwalin12@berkeley.edu>
2022-01-12 15:42:51 -0800

    contract addresses

---
+++ src/contract_addresses.ts
@@ -0,0 +1,10 @@
+//deployed on Goerli
+
+
+export const ownerAddress = "0x652E3fA6353de83ac2b667368E75FEec05e9d5A9";
+export const rentPoolFactoryAddress = "0x215a56BcC26653C554cF63ddefD3fD329ba9Ebd0";
+export const blackScholesAddress =  "0x1703C366F7EAFa364D1054c9DdB08326f183D9c6";
+export const greekCacheAddress = "0x564188DC9106CBD1738D44E6C243542A1C2535c0";
+export const rentPlatformAddress = "0x245bF791c1062F50960a9e49F3dD3bE2DDBA9011";
+export const rentEscrowAddress = "0x5E224ADa02106f77d57DBc1dadDe658Be906CD63";
+export const caravanRouterAddress = "0xaa296c1C0e3328F76E04AFa704B6b6c05bb3B177";
